caa7116b8772d046578a0786fa884aca213a0c1a
lukuenya <46133049+lukuenya@users.noreply.github.com>
2023-07-12 10:14:41 +0200

    some preprocessing steps

--- data_analysis/useful_functions.py
+++ data_analysis/useful_functions.py
@@ -4,6 +4,8 @@
 
 ##### Renaming a directory  `march` as `gait`
 import os 
+import re
+import shutil
 
 def rename_subfolder(root_folder, old_name, new_name):
     # Iterate over all directories and files in the root directory
@@ -54,6 +56,139 @@ def load_data(subfolder_name):
 
 # Usage
 data_marche, data_posture = load_data('foldername') # Replace foldername 
+
+
+# -----------------------------------------------------------------------------------------------------
+
+#I want a list of the first-level subfolders containing more than two files with names starting with a date and hour in a format like `2017-09-21_08_22_12_`
+main_folder = "P:/DATA_OCT_22/stade_2_Global_dataset/New/"
+date_hour_pattern = re.compile(r"^\d{4}-\d{2}-\d{2}_\d{2}_\d{2}_\d{2}_")
+string_pattern = re.compile(r"ACQ_")
+first_level_subfolder = []
+
+for entry in os.scandir(main_folder):
+    if entry.is_dir():
+        matched_files_count = 0
+        for item in os.scandir(entry.path):
+            if item.is_file() and date_hour_pattern.match(item.name):
+                matched_files_count +=1
+
+        if matched_files_count == 2 :
+            first_level_subfolder.append(entry.path)
+
+print(first_level_subfolder)
+
+
+# --------------------------------------------------------------------------------------------------------
+
+# Moving `posture` and `marche` data in theirs folders respectively
+main_folder = "P:/DATA_OCT_22/stade_2_Global_dataset/New/"
+date_hour_pattern = re.compile(r"^\d{4}-\d{2}-\d{2}_\d{2}_\d{2}_\d{2}_")
+acq_pattern = re.compile(r"^ACQ_")
+
+for entry in os.scandir(main_folder):
+    if entry.is_dir():
+        posture_folder = os.path.join(entry.path, "posture")
+        marche_folder = os.path.join(entry.path, "march")
+
+        # Create posture and march folders if they don't exist
+        os.makedirs(posture_folder, exist_ok=True)
+        os.makedirs(marche_folder, exist_ok=True)
+
+        for item in os.scandir(entry.path):
+            if item.is_file():
+                if date_hour_pattern.match(item.name):
+                    shutil.move(item.path, os.path.join(posture_folder, item.name))
+
+                elif acq_pattern.match(item.name):
+                    shutil.move(item.path, os.path.join(marche_folder, item.name))
+
+print('ok')
+
+# ----------------------------------------------------------------------------------------------------------
+
+source_folder = 'P:/DATA_OCT_22/stade_2_Global_dataset/New/'
+destination_folder = 'P:/DATA_OCT_22/posture_copy/ici/'
+
+#Get the first level subfolder in both directories
+source_subfolders = [name for name in os.listdir(source_folder) if os.path.isdir(os.path.join(source_folder, name))]
+
+destination_subfolders = [name for name in os.listdir(destination_folder) if os.path.isdir(os.path.join(destination_folder, name))]
+
+# Check the match between the 2 subfolders
+matching_subfolders = set(source_subfolders) & set(destination_subfolders)
+
+for subfolder in matching_subfolders:
+    for root, dirs, files in os.walk(os.path.join(source_folder, subfolder)):
+        # Check if 'features' is in the directories
+        if 'Features' in dirs:
+            features_dir = os.path.join(root, 'Features')
+
+            # check for 'feat2017-09-21_08_22_12.json' and 'featXsens.json' in the 'features' directory
+            if 'feat2017-09-21_08_22_12.json' in os.listdir(features_dir):
+                shutil.copytree(features_dir, os.path.join(destination_folder, subfolder, 't0', 'posture', 'Features'), dirs_exist_ok=True)
+
+            elif 'featXsens.json' in os.listdir(features_dir):
+                shutil.copytree(features_dir, os.path.join(destination_folder, subfolder, 't0', 'gait', 'Features'), dirs_exist_ok=True)
+            
+
+# ----------------------------------------------------------------------------------------------------------
+
+feat_pattern = re.compile(r"^feat")
+
+for entry in os.scandir(destination_folder):
+    if entry.is_dir():
+        feature_folder = os.path.join(destination_folder, entry, 't0', 'gait', 'Features')
+
+        os.makedirs(feature_folder, exist_ok=True)
+
+        for item in os.walk(os.path.join(destination_folder, entry, 't0' )):
+            if item.is_file():
+                if feat_pattern.match(item.name):
+                    shutil.move(item.path, os.path.join(feature_folder, item.name))
+# -------------------------------------------------------------------------------------------------------
+
+# Dro all the empty folders
+main_folder = "./copy_v1/"
+
+for entry in os.scandir(main_folder):
+    if entry.is_dir():
+        posture_folder = os.path.join(entry.path, "posture")
+        marche_folder = os.path.join(entry.path, "march")
+
+        if os.path.exists(posture_folder) and not any(os.scandir(posture_folder)):
+            shutil.rmtree(posture_folder)
+
+        if os.path.exists(marche_folder) and not any(os.scandir(marche_folder)):
+            shutil.rmtree(marche_folder)
+
+print('removed')
+
+# -------------------------------------------------------------------------------------------------
+
+# Move `posture` folder in t0 and `march` in t1
+main_folder = "./copy_v1 - Copy/"
+
+for entry in os.scandir(main_folder):
+    if entry.is_dir():
+        t0_folder = os.path.join(entry.path, "t0")
+        t1_folder = os.path.join(entry.path, "t1")
+
+        posture_folder = os.path.join(entry.path, "posture")
+        marche_folder = os.path.join(entry.path, "march")
+
+        os.makedirs(t0_folder, exist_ok=True)
+        os.makedirs(t1_folder, exist_ok=True)
+
+        if os.path.exists(posture_folder):
+            shutil.move(posture_folder, os.path.join(t0_folder, "posture"))
+
+        if os.path.exists(marche_folder):
+            shutil.move(marche_folder, os.path.join(t0_folder, "march"))
+
+print('ok')
+
+
 
 
 
